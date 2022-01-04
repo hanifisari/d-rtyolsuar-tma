18c038f4b686c0867ee4cb22646b04d6bfbc1beb
hanifisari <1612568+hanifisari@users.noreply.github.com>
2022-01-04 18:24:01 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -281,3 +281,39 @@
   });
 
 })()
+
+ /**
+   * Clients Slider
+   */
+  new Swiper('.clients-slider', {
+    speed: 500,
+    loop: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 40
+      },
+      480: {
+        slidesPerView: 3,
+        spaceBetween: 60
+      },
+      640: {
+        slidesPerView: 4,
+        spaceBetween: 80
+      },
+      992: {
+        slidesPerView: 6,
+        spaceBetween: 120
+      }
+    }
+  });
